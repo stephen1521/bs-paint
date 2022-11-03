c886da621ffe073ceaa09df9249fa1db9534ef52
stephen1521 <stephen.warnock@codeimmersives.com>
2022-11-03 13:50:31 -0400

    updating

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
  * To make the second one happen, the number to change
  * is the first argument to `repeat`, currently set at 10.
  */
-const gridWidth = 10;
+const gridWidth = 30;
 let count = 0;
 while (count <= gridWidth * gridWidth) {
   const canvas = document.querySelector('.canvas');
@@ -48,8 +48,14 @@ while (count <= gridWidth * gridWidth) {
 
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
+let colorList = document.querySelectorAll('.palette .palette-color');
 
+let canvas = document.querySelector('.canvas');
+let canvasDiv = canvas.querySelectorAll('.square');
 
+let brush = document.querySelector('.current-brush');
+
+let mouseDown = false;
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
@@ -60,8 +66,36 @@ while (count <= gridWidth * gridWidth) {
 // empty at first, though a console.log just to know they're being
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
+for(let i = 0; i < colorList.length; i++){
+  colorList[i].addEventListener('click', function(){
+    brush.classList.replace(brush.classList[1], colorList[i].classList[1]);
+  });
+}
+
+canvas.addEventListener('click', function(e){
+  let element = e.target;
+  if(element.localName = 'div'){
+    element.classList.replace(element.classList[1], brush.classList[1]);
+  }
+  mouseDown = false;
+});
 
 
+for(let i of canvasDiv){
+  i.addEventListener('mouseenter', function(){
+    if(mouseDown){
+      i.classList.replace(i.classList[1], brush.classList[1]);
+    }
+  });
+}
+
+document.querySelector('body').addEventListener('mouseup', function(){
+  mouseDown = false;
+});
+
+document.querySelector('body').addEventListener('mousedown', function(){
+  mouseDown = true;
+});
 
 /**************************
  * WIRING IT ALL TOGETHER *
